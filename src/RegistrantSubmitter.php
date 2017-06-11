e84d6bc97c359a78e0df012c1005d314fe503578
Jefferson <jefferson.dab@gmail.com>
2017-06-10 22:19:43 -0300

    Fixing the content-type header

--- src/RegistrantSubmitter.php
+++ src/RegistrantSubmitter.php
@@ -7,11 +7,12 @@ class RegistrantSubmitter
     $curl = curl_init();
 
     curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HEADER, 'application/json');
+    curl_setopt($curl, CURLOPT_HEADER, true);
     curl_setopt($curl, CURLOPT_URL, $location);
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($lead->toArray()));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
       'X-Lasso-Auth:'. 'Token='. $apiKey . ',Version=1.0',
     ));
     curl_exec($curl);
